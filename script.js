005721f028d291d35d7f341d8110510c0039a27e
Ciska van der merwe <110846811+minislinket@users.noreply.github.com>
2023-02-17 21:43:55 +0200

    Update script.js
    fixed a typo on image.checked that made my auto carousel not work

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let counter = 1;
 setInterval(function(){
-document.getElementById('radio' + counter).checker = true;
+document.getElementById('radio' + counter).checked = true;
 counter++
 if(counter > 4){
     counter = 1;
